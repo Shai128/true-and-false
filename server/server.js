87676fbfb1e764a06e1934b5972a73cd386ff4e5
Shai128 <shai120899@gmail.com>
2019-11-02 19:10:35 +0200

    home page, router

--- server/server.js
+++ server/server.js
@@ -8,12 +8,14 @@ bodyParser = require("body-parser")
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:8000"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", 'Content-Type');
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     next();
   });
+  
 
 
 
@@ -23,7 +25,6 @@ app.get('/welcome',(req,res)=> res.send(`hello ${req.body.shalom.bla}`))
 
 app.post('/user',(req,res)=> {
     let data = JSON.parse(req.body.json)
-    console.log(data.password)
     createUser(data.username, data.password,
     ()=>{res.send("success")},(err)=>{console.log(err)});}
      
--- src/App.js
+++ src/App.js
@@ -1,59 +1,291 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Avatar from '@material-ui/core/Avatar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+function Copyright() {
+  return (
+ <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="">
+        True and False
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+   
+  );
+}
 
-  /*style things */
-  const useTextStyles = makeStyles(theme => ({
-    container: {
-      display: 'flex',
-      flexWrap: 'wrap',
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white,
     },
-    textField: {
-      marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(1),
-      width: 200,
-    },
-  }));
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+  root: {
+    width: '100%',
+    maxWidth: 500,
+    color: '#000000'
+  }
+}));
 
-  const useButtonStyles = makeStyles(theme => ({
-    button: {
-      margin: theme.spacing(1),
-    },
-    input: {
-      display: 'none',
-    },
-  }));
 
-  const useTypographyStyles = makeStyles({
-    root: {
-      width: '100%',
-      maxWidth: 500,
-      color: '#000000'
-    },
-  });
 
-  const textClasses = useTextStyles();
-  const buttonClasses = useButtonStyles();
-  const typographyClasses = useTypographyStyles();
+function SignUp() {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="userName"
+                label="User Name"
+                name="userName"
+                autoComplete="userName"
+              />
+              </Grid>
+              <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="nickName"
+                label="Nick Name"
+                name="nickName"
+                autoComplete="nickName"
+              />
+              </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I want to receive inspiration, marketing promotions and updates via email."
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link to="/SignIn" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      
+    </Container>
+    
+  );
+}
+
+function LinksPage(){
+  return (
+    <div>
+      <Switch>
+      <Route exact path="/">
+            <Home />
+          </Route>
+          
+         <Route exact path="/SignIn">
+            <SignIn />
+          </Route>
+          <Route exact path="/SignUp">
+            <SignUp />
+          </Route>
+          
+        </Switch>
+        </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+{/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+  
+    <LinksPage />
+  </Router>
+        
+         
+  );
+}
+
+function Home(){
+  const classes = useStyles();
+
+
+  return (
+    <Container component="main" maxWidth="md">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h2">
+          True and False Home Page
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+            <Link to="/SignIn">
+            <Button variant="contained" color="primary" fullWidth  className={classes.button}>
+            Sign In
+            </Button>
+            </Link>
+
+            </Grid>
+            <Grid item xs={12} sm={6}>
+            <Link to="/SignUp">
+            <Button variant="contained" color="primary" fullWidth  className={classes.button}>
+      Sign Up
+      </Button>
+      </Link>
+            </Grid>
+            
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+
+    </Container>
+    
+  );
+
+}
+
+function SignIn() {
+
+
+  const classes = useStyles();
 
 
   return (
     <div className="App" >
       <header className="App-header" >
         <div>
-        <Typography variant="h1" component="h2" gutterBottom className={typographyClasses.root}>
-        Sign Up
+        <Typography variant="h1" component="h2" gutterBottom className={classes.root}>
+        Sign In
       </Typography>
 
         <TextField
           id="UserNameInput"
-          className={textClasses.textField}
+          className={classes.textField}
           label="Username"
           margin="normal"
           variant="filled"
@@ -62,7 +294,7 @@ function App() {
         <TextField
           id="PasswordInput"
           label="Password"
-          className={textClasses.textField}
+          className={classes.textField}
           type="password"
           autoComplete="current-password"
           margin="normal"
@@ -70,7 +302,7 @@ function App() {
         />
         </div>
       
-        <Button variant="contained" color="primary" className={buttonClasses.button}
+        <Button variant="contained" color="primary" className={classes.button}
         onClick={()=>{
           let user = {username :document.getElementById('UserNameInput').value,
                       password : document.getElementById('PasswordInput').value }
@@ -85,7 +317,7 @@ function App() {
           body: 'json='+JSON.stringify( user )
         });
       }}>   
-        Sign Up
+        Sign In
       </Button>
       
 
@@ -94,5 +326,6 @@ function App() {
     </div>
   );
 }
-
+/*
 export default App;
+*/
