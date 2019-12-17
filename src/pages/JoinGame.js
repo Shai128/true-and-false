77f940deab5d8621d32857f6ec392a9f396ed0c5
Kalifa <halifadan@gmail.com>
2019-12-17 11:39:19 +0200

    full virsion that work without reading data from the server in the game

--- src/pages/JoinGame.js
+++ src/pages/JoinGame.js
@@ -39,10 +39,6 @@ import {
   Redirect,
   useHistory,
 } from "react-router-dom";
-// import FadeIn from "react-fade-in";
-// import Lottie from "react-lottie";
-// import ReactLoading from "react-loading";
-// import "bootstrap/dist/css/bootstrap.css";
 import {ChatButton} from './../PagesUtils.js';
 const okStatus = 200;
 
@@ -94,6 +90,7 @@ socket.on("userJoined", function(userInfo) {
  /*
     userInfo: {email: ..., nickName:...}
  */
+
     console.log("userINFO DANN --> ",userInfo);
     console.log("LIST DANN --> ",PlayersAvailable.values);
 
@@ -393,160 +390,6 @@ export function BasicTextFields() {
     </form>
   );
 }
-
-
-
-
-
-
-
-
-// ----------------------THE STEPER--------------------------------- //
-const useStylesSteper = makeStyles(theme => ({
-  root: {
-    width: '90%',
-  },
-  button: {
-    marginRight: theme.spacing(1),
-  },
-  instructions: {
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(1),
-  },
-}));
-
-function getSteps() {
-  return ['Home Page', 'Join a game', 'Play!'];
-}
-
-function getStepContent(step) {
-  switch (step) {
-    case 0:
-      return '';
-    case 1:
-      return '';
-    case 2:
-      return '';
-    default:
-      return '';
-  }
-}
-
-export function HorizontalLinearStepper() {
-  const classes = useStylesSteper();
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [skipped, setSkipped] = React.useState(new Set());
-  const steps = getSteps();
-
-  const isStepOptional = step => {
-    return step === 1;
-  };
-
-  const isStepSkipped = step => {
-    return skipped.has(step);
-  };
-
-  const handleNext = () => {
-    let newSkipped = skipped;
-    if (isStepSkipped(activeStep)) {
-      newSkipped = new Set(newSkipped.values());
-      newSkipped.delete(activeStep);
-    }
-
-    setActiveStep(prevActiveStep => prevActiveStep + 1);
-    setSkipped(newSkipped);
-  };
-
-  const handleBack = () => {
-    setActiveStep(prevActiveStep => prevActiveStep - 1);
-  };
-
-  const handleSkip = () => {
-    if (!isStepOptional(activeStep)) {
-      // You probably want to guard against something like this,
-      // it should never occur unless someone's actively trying to break something.
-      throw new Error("You can't skip a step that isn't optional.");
-    }
-
-    setActiveStep(prevActiveStep => prevActiveStep + 1);
-    setSkipped(prevSkipped => {
-      const newSkipped = new Set(prevSkipped.values());
-      newSkipped.add(activeStep);
-      return newSkipped;
-    });
-  };
-
-  const handleReset = () => {
-    setActiveStep(0);
-  };
-
-  return (
-    <div className={classes.root}>
-      <Stepper activeStep={activeStep}>
-        {steps.map((label, index) => {
-          const stepProps = {};
-          const labelProps = {};
-          if (isStepOptional(index)) {
-            labelProps.optional = <Typography variant="caption"></Typography>;
-          }
-          if (isStepSkipped(index)) {
-            stepProps.completed = false;
-          }
-          return (
-            <Step key={label} {...stepProps}>
-              <StepLabel {...labelProps}>{label}</StepLabel>
-            </Step>
-          );
-        })}
-      </Stepper>
-      <div>
-        {activeStep === steps.length ? (
-          <div>
-            {/* <Typography className={classes.instructions}>
-              All steps completed - you&apos;re finished
-            </Typography> */}
-            {/* <Button onClick={handleReset} className={classes.button}>
-              Reset
-            </Button> */}
-          </div>
-        ) : (
-          <div>
-            <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
-            <div>
-              <Button disabled={activeStep === 0} onClick={handleBack} className={classes.button}>
-                Back
-              </Button>
-              {/* {isStepOptional(activeStep) && (
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={handleSkip}
-                  className={classes.button}
-                >
-                  Skip
-                </Button>
-              )} */}
-
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={handleNext}
-                className={classes.button}
-              >
-                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-              </Button>
-            </div>
-          </div>
-        )}
-      </div>
-    </div>
-  );
-}
-
-// ------------------------------------------------------------------- //
-
-
-
 
 // ------------------ LIST OF PLAYERS IN THE ROOM ------------------ //
 
