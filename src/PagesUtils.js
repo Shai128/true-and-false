880d123fe6ff87f89c5ad7d76808403765595194
Shai128 <shai120899@gmail.com>
2020-01-01 16:20:10 +0200

    recent games in LoginScreenHome and GamePage now read data from DB

--- src/PagesUtils.js
+++ src/PagesUtils.js
@@ -10,10 +10,10 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography'
 import {
-      Link,
-      useHistory,
-  } from "react-router-dom";
-import {isUndefined} from './Utils.js';
+  Link,
+  useHistory,
+} from "react-router-dom";
+import { isUndefined } from './Utils.js';
 
 import ChatIcon from '@material-ui/icons/Chat';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -26,43 +26,43 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import {socket, getUserFromProps, getCurrentUserFromDB} from './user.js';
-import {useStyles} from './App.js';
-import {joinRoom} from './room.js';
-export function PrintGames(props){
-    const games =props.games;
-    const classes = props.classes;
-    const user = getUserFromProps(props);
-    let url = props.url;
-    return (<List className={classes.list}>
-          {games.map(({ name, id, date, playersNum }) => (
-            <React.Fragment key={id}>
-              <Link to={
-                {
-                  pathname: `${url}/GamePage/`+id,
-                  game: games[id],
-                  user: user,
-                }
-              }
-              >
-              <ListItem button  className={classes.gamesListItems}>
-                <ListItemText primary={name} secondary={date} />
-              </ListItem>
-              </Link>
-            </React.Fragment>
-          ))}
-        </List>);
+import { socket, getUserFromProps, getCurrentUserFromDB } from './user.js';
+import { useStyles } from './App.js';
+import { joinRoom } from './room.js';
+export function PrintGames(props) {
+  const games = props.games;
+  const classes = props.classes;
+  const user = getUserFromProps(props);
+  let url = props.url;
+  return (<List className={classes.list}>
+    {games.map(({ room_name, id, date }) => (
+      <React.Fragment key={id}>
+        <Link to={
+          {
+            pathname: `${url}/GamePage/` + id,
+            game: games[id],
+            user: user,
+          }
+        }
+        >
+          <ListItem button className={classes.gamesListItems}>
+            <ListItemText primary={room_name} secondary={date} />
+          </ListItem>
+        </Link>
+      </React.Fragment>
+    ))}
+  </List>);
 }
 
-export function DisplayLoading(){
+export function DisplayLoading() {
   return (
     <div
-     style={{
+      style={{
         position: 'absolute', left: '50%', top: '50%',
         transform: 'translate(-50%, -50%)'
-    }}>
+      }}>
 
-  <ReactLoading type={"bars"} color={"#000"} height={667} width={375} />
+      <ReactLoading type={"bars"} color={"#000"} height={667} width={375} />
     </div>
   );
 }
@@ -71,37 +71,37 @@ export function DisplayLoading(){
 /**
  * display a db error message and displaying a return-back-to-home button
  */
-export function DisplayDBError(){
+export function DisplayDBError() {
 
 
   return (
 
     <div className="App" >
-          <header className="App-header" >
-          <Container component="main" maxWidth="xs">
-            <CssBaseline />
+      <header className="App-header" >
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
 
-              <Typography variant="h4" style={{color:'black', marginBottom: '30px', width:'100%'}}>
-              An error occured. Could not read from DB.
+          <Typography variant="h4" style={{ color: 'black', marginBottom: '30px', width: '100%' }}>
+            An error occured. Could not read from DB.
               </Typography>
 
-              <Link to="/LoginScreen">
-                <Button variant="contained" color="primary" fullWidth>
-                Return to home screen
+          <Link to="/LoginScreen">
+            <Button variant="contained" color="primary" fullWidth>
+              Return to home screen
             </Button>
-              </Link> 
+          </Link>
 
-          </Container>
-          </header>
-          
-          </div>
+        </Container>
+      </header>
+
+    </div>
   );
 }
 
 
 
-export function PrintJoinGameDialog(props){  
-  const {handleCloseWindow,  WindowOpen, currentUser} = props;
+export function PrintJoinGameDialog(props) {
+  const { handleCloseWindow, WindowOpen, currentUser } = props;
   const [gameID, setGameID] = React.useState("");
   const [currentGameNickName, setCurrentGameNickName] = React.useState(currentUser.nickName);
   const [validGameID, setValidGameID] = React.useState(true);
@@ -110,103 +110,103 @@ export function PrintJoinGameDialog(props){
   const [gameIDHelperText, setGameIDHelperText] = React.useState('');
 
 
-  const onCloseWindow = ()=>{
+  const onCloseWindow = () => {
     resetDisplaysContent();
     handleCloseWindow();
   }
 
-  const displayNickNameTaken = ()=>{
+  const displayNickNameTaken = () => {
     setNickNameHelperText('This nick name is taken');
     setvalidNickName(false);
   }
 
-  const displayWrongGameID = ()=>{
+  const displayWrongGameID = () => {
     setGameIDHelperText('Wrong game ID');
     setValidGameID(false);
   }
 
-  const resetDisplaysContent = ()=>{
+  const resetDisplaysContent = () => {
     setGameIDHelperText('');
     setNickNameHelperText('');
     setvalidNickName(true);
     setValidGameID(true);
   }
-  socket.on('joinedRoom', function(roomID){
+  socket.on('joinedRoom', function (roomID) {
     //todo- redirect to room page (dan't page)
   });
 
-  socket.on('nickNameTaken', function(){
+  socket.on('nickNameTaken', function () {
     displayNickNameTaken();
     //todo- open message 'try a different name'
   });
 
-  socket.on('wrongRoomID', function(){
+  socket.on('wrongRoomID', function () {
     displayWrongGameID();
 
   });
   let history = useHistory();
-  const joinGame = ()=>{
-/*
-    //var user = //todo- get user from session
-    var roomData = {
-      roomName: gameName,
-      user: user
-    }
-
+  const joinGame = () => {
+    /*
+        //var user = //todo- get user from session
+        var roomData = {
+          roomName: gameName,
+          user: user
+        }
     
-    var user = {
-      //todo- get socketID from session
-      //socketID: ,
-      gameNickName: currentGameNickName
-    }
-    var data = {
-      user: user,
-      roomID: gameID
-    }
+        
+        var user = {
+          //todo- get socketID from session
+          //socketID: ,
+          gameNickName: currentGameNickName
+        }
+        var data = {
+          user: user,
+          roomID: gameID
+        }
+    
+        socket.emit('joinRoom', data);
+    */
 
-    socket.emit('joinRoom', data);
-*/
-
-      console.log("starting game!");
-      console.log("game ID:", gameID);
-      console.log('user nickname: ', currentGameNickName);
-      joinRoom(gameID, currentUser, currentGameNickName, history )
+    console.log("starting game!");
+    console.log("game ID:", gameID);
+    console.log('user nickname: ', currentGameNickName);
+    joinRoom(gameID, currentUser, currentGameNickName, history)
   }
-  return(
-      <Dialog id = "openRoomPopUp" open={WindowOpen} onClose={onCloseWindow} aria-labelledby="form-dialog-title">
+  return (
+    <Dialog id="openRoomPopUp" open={WindowOpen} onClose={onCloseWindow} aria-labelledby="form-dialog-title">
       <DialogTitle id="form-dialog-title">Join a Room</DialogTitle>
       <DialogContent>
         <DialogContentText>
         </DialogContentText>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-        <TextField
-          error = {!validGameID}
-          helperText={gameIDHelperText}
-          autoFocus
-          margin="dense"
-          id="room_id"
-          label="Room ID"
-          onChange={(event)=>{
-            setGameID(event.target.value);
-          }}
-        />
-        </Grid>
-        <Grid item xs={12}>
+            <TextField
+              error={!validGameID}
+              helperText={gameIDHelperText}
+              autoFocus
+              margin="dense"
+              id="room_id"
+              label="Room ID"
+              onChange={(event) => {
+                setGameID(event.target.value);
+              }}
+            />
+          </Grid>
+          <Grid item xs={12}>
 
-        <TextField
-          error={!validNickName}
-          helperText={nickNameHelperText}
-          autoFocus
-          margin="dense"
-          id="nickName"
-          label="Nick Name"
-          defaultValue={currentUser.nickName}
-          onChange={(event)=>{
-              setCurrentGameNickName(event.target.value);
-          }}
-        />
-        </Grid>
+            <TextField
+              error={!validNickName}
+              helperText={nickNameHelperText}
+              autoFocus
+              margin="dense"
+              id="nickName"
+              label="Nick Name"
+              defaultValue={currentUser.nickName}
+              onChange={(event) => {
+                setCurrentGameNickName(event.target.value);
+              }}
+            />
+          </Grid>
         </Grid>
       </DialogContent>
       <DialogActions>
@@ -214,7 +214,7 @@ export function PrintJoinGameDialog(props){
           Cancel
         </Button>
         <Button id="startBTN" onClick={joinGame} color="primary">
-              Start
+          Start
         </Button>
       </DialogActions>
     </Dialog>
@@ -222,128 +222,131 @@ export function PrintJoinGameDialog(props){
   );
 }
 
-export function PrintMessages(props){
-  
-    const useStyles = makeStyles(theme => ({
-      root: {
-        width: '100%',
-        maxWidth: 360,
-        overflowX: 'hidden', overflowY: "auto",
-        backgroundColor: theme.palette.background.paper,
-        overflowWrap: 'break-word',
-      },
-    }));
-    const classes = useStyles();
-    const url = props.url;
-    const messages =props.messages;
-    const user = getUserFromProps(props);
-    var index =0;
-    const shortMessage = (message) =>{
-      if(message.length >10)
-        return message.slice(0,10)+'...';
-      return message;
-    }
-    console.log('messages: ', messages);
-    let history = useHistory();
-    return (
-      <div className={classes.root}>
-        <List >
-          {messages.map((message) => (
-            <React.Fragment key={index++}>
-              <ListItem className={classes.root} button onClick={()=>{
-                history.push({
-                  pathname: `${url}/ChatRoom/`+message.authorEmail,
-                  user: user,
-                })
-              }}>
-                <ListItemText primary={message.authorName + ": "+shortMessage(message.messageContent)} /*secondary={date}*/ />
-              </ListItem>
-            </React.Fragment>
-          ))}
-        </List>
-      </div>
-        );
+export function PrintMessages(props) {
+
+  const useStyles = makeStyles(theme => ({
+    root: {
+      width: '100%',
+      maxWidth: 360,
+      overflowX: 'hidden', overflowY: "auto",
+      backgroundColor: theme.palette.background.paper,
+      overflowWrap: 'break-word',
+    },
+  }));
+  const classes = useStyles();
+  const url = props.url;
+  const messages = props.messages;
+  const user = getUserFromProps(props);
+  var index = 0;
+  const shortMessage = (message) => {
+    if (message.length > 10)
+      return message.slice(0, 10) + '...';
+    return message;
+  }
+  console.log('messages: ', messages);
+  let history = useHistory();
+  return (
+    <div className={classes.root}>
+      <List >
+        {messages.map((message) => (
+          <React.Fragment key={index++}>
+            <ListItem className={classes.root} button onClick={() => {
+              history.push({
+                pathname: `${url}/ChatRoom/` + message.authorEmail,
+                user: user,
+              })
+            }}>
+              <ListItemText primary={message.authorName + ": " + shortMessage(message.messageContent)} /*secondary={date}*/ />
+            </ListItem>
+          </React.Fragment>
+        ))}
+      </List>
+    </div>
+  );
 
 }
 
 
 
-export function PrintChats(props){
-  var messeges_by_addressee  = JSON.parse(JSON.stringify(props.chats));
-  
+export function PrintChats(props) {
+  var messeges_by_addressee = JSON.parse(JSON.stringify(props.chats));
+
   const classes = useStyles();
   let history = useHistory();
-  if(isUndefined(messeges_by_addressee) || messeges_by_addressee.length ===0)
-    messeges_by_addressee=[];
+  if (isUndefined(messeges_by_addressee) || messeges_by_addressee.length === 0)
+    messeges_by_addressee = [];
   const user = getUserFromProps(props);
-  let index =0;
+  let index = 0;
   var chats = [];
-  for(let chat of messeges_by_addressee){
-    let last_message = chat.messages[ chat.messages.length-1];
-    last_message.delivery_timestamp =  Date.parse(last_message.delivery_timestamp)
+  for (let chat of messeges_by_addressee) {
+    let last_message = chat.messages[chat.messages.length - 1];
+    last_message.delivery_timestamp = Date.parse(last_message.delivery_timestamp)
     chats.push(last_message);
     console.log(last_message);
   }
-  chats.sort((message1, message2)=>{return message1.delivery_timestamp<message2.delivery_timestamp? 1: -1});
+  chats.sort((message1, message2) => { return message1.delivery_timestamp < message2.delivery_timestamp ? 1 : -1 });
   return (<List className={classes.list}>
-        {chats.map((chat) => (
-          
-          <React.Fragment key={index++}>
-            
-            <ListItem button  onClick={()=>{
-            history.push({
-              pathname: `/LoginScreen/ChatRoom/`+chat.otherUserEmail,
-              user: user,
-            })
-          }} >
-              <ListItemText primary={chat.otherUserEmail} secondary={
-                
-                (chat.authorEmail === user.email? 'You: ' : (chat.authorName +": ")) +
-                  chat.messageContent
-                } />
-            </ListItem>
-          </React.Fragment>
-        ))}
-      </List>);
+    {chats.map((chat) => (
+
+      <React.Fragment key={index++}>
+
+        <ListItem button onClick={() => {
+          history.push({
+            pathname: `/LoginScreen/ChatRoom/` + chat.otherUserEmail,
+            user: user,
+          })
+        }} >
+          <ListItemText primary={chat.otherUserEmail} secondary={
+
+            (chat.authorEmail === user.email ? 'You: ' : (chat.authorName + ": ")) +
+            chat.messageContent
+          } />
+        </ListItem>
+      </React.Fragment>
+    ))}
+  </List>);
 }
 
 /**
  * 
  * @param {email - the email of the other user. user: current user (not neccessary)} props 
  */
-export function ChatButton(props){
+export function ChatButton(props) {
   let history = useHistory();
   return (
-      <ListItem button onClick={()=>{history.push({
-        pathname: `/LoginScreen/ChatRoom/`+props.email,
+    <ListItem button onClick={() => {
+      history.push({
+        pathname: `/LoginScreen/ChatRoom/` + props.email,
         user: props.user
-      })}}>
-         
+      })
+    }}>
+
       <Grid container alignContent='center' direction='column' alignItems='center' justify='center'>
-      <Grid item xs={5}>
-        <ListItemIcon >
-          <ChatIcon />
-        </ListItemIcon>
+        <Grid item xs={5}>
+          <ListItemIcon >
+            <ChatIcon />
+          </ListItemIcon>
         </Grid>
         <Grid item xs={7}>
-        <ListItemText primary="Chat"/>
+          <ListItemText primary="Chat" />
         </Grid>
-        </Grid>
-        </ListItem>
-      
+      </Grid>
+    </ListItem>
+
   );
 }
 
-export function AutoRedirectToLoginScreenIfUserInSession(history){
-  getCurrentUserFromDB(()=>{}, ()=>{
+export function AutoRedirectToLoginScreenIfUserInSession(history) {
+  getCurrentUserFromDB(() => { }, () => {
 
     var locationArr = history.location.pathname.split("/");
     var len = locationArr.length;
-    var page = locationArr[len-1];
-    if( page === 'SignIn' || page === '' || page === 'SignUp'){
+    var page = locationArr[len - 1];
+    if (page === 'SignIn' || page === '' || page === 'SignUp') {
       history.push({
         pathname: '/LoginScreen',
       });
 
-  }});
+    }
+  });
 }
--- src/pages/LoginScreenHome.js
+++ src/pages/LoginScreenHome.js
@@ -16,29 +16,29 @@ import clsx from 'clsx';
 import Divider from '@material-ui/core/Divider';
 
 import {
-    useHistory,
-    useRouteMatch,
-    Switch,
-    Route,
-  } from "react-router-dom";
-import {GamePage} from './GamePage.js';
-import {userIsUpdated, getCreatedGames, getParticipatedGames, getCurrentUserFromSession as getCurrentUser, getUserFromProps} from './../user';
-import {PrintGames, PrintJoinGameDialog, DisplayLoading} from './../PagesUtils';
-import {createRoom} from './../room.js'
-export function LoginScreenHome(props){
-    let { path, url } = useRouteMatch();
-    let user = getUserFromProps(props);
-    return(
-        <Switch>
+  useHistory,
+  useRouteMatch,
+  Switch,
+  Route,
+} from "react-router-dom";
+import { GamePage } from './GamePage.js';
+import { userIsUpdated, getCreatedGames, getParticipatedGames, getCurrentUserFromSession as getCurrentUser, getUserFromProps } from './../user';
+import { PrintGames, PrintJoinGameDialog, DisplayLoading } from './../PagesUtils';
+import { createRoom } from './../room.js'
+export function LoginScreenHome(props) {
+  let { path, url } = useRouteMatch();
+  let user = getUserFromProps(props);
+  return (
+    <Switch>
 
-        <Route exact path={path}>
-        <Home path = {path} url = {url} user={user}/>
-        </Route>
+      <Route exact path={path}>
+        <Home path={path} url={url} user={user} />
+      </Route>
 
-        <Route path={`${path}/GamePage/:id`} exact component={GamePage} user={user} />
-        </Switch>
+      <Route path={`${path}/GamePage/:id`} exact component={GamePage} user={user} />
+    </Switch>
 
-);
+  );
 }
 
 function Home(props) {
@@ -92,14 +92,14 @@ function Home(props) {
   }));
 
 
-    const classes = useStyles();
-    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
-    const [currentUser, setCurrentUser] = React.useState(getUserFromProps(props));
-    getCurrentUser(currentUser, setCurrentUser);
-    
-    if(!userIsUpdated(currentUser)){
-      return (<DisplayLoading/>);
-    }
+  const classes = useStyles();
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+  const [currentUser, setCurrentUser] = React.useState(getUserFromProps(props));
+  getCurrentUser(currentUser, setCurrentUser);
+
+  if (!userIsUpdated(currentUser)) {
+    return (<DisplayLoading />);
+  }
 
   if (!userIsUpdated(currentUser)) {
     return (<DisplayLoading />);
@@ -114,8 +114,8 @@ function Home(props) {
               <Grid container spacing={3}>
                 <Grid item xs={12}>
 
-            <Typography id="welcomeMessage" component="h1" variant="h2" justify="center">
-              Welcome {currentUser.firstName}!
+                  <Typography id="welcomeMessage" component="h1" variant="h2" justify="center">
+                    Welcome {currentUser.firstName}!
         </Typography>
                 </Grid>
                 <Grid item xs={12} sm={6}>
@@ -146,32 +146,47 @@ function Home(props) {
           </Grid>
 
           <Grid item xs={12}>
-          <Paper className={fixedHeightPaper}>
-            <Grid container spacing={3}>
-            <Grid item xs={12}>
+            <Paper className={fixedHeightPaper}>
+              <Grid container spacing={3}>
+                <Grid item xs={12}>
 
-            <Typography component="h4" variant="h4" justify="center">
-            Recent Games
+                  <Typography component="h4" variant="h4" justify="center">
+                    Recent Games
         </Typography>
-          <Divider/>
-            </Grid>
+                  <Divider />
+                </Grid>
 
-            <Grid item xs={12}>
-                <ShowLastTwoGames url={url} />
-            </Grid>
+                <Grid item xs={12}>
+                  <ShowLastTwoGames user={currentUser} url={url} />
+                </Grid>
 
-            </Grid>
+              </Grid>
             </Paper>
-            </Grid>
-            </Grid>
+          </Grid>
+        </Grid>
 
-          </Container>
-</div>
-    );
+      </Container>
+    </div>
+  );
 
 }
-function getTwoRecentGames() {
-  let participated = getParticipatedGames().slice(0);
+
+function getTwoRecentGames(user) {
+  let participated = user.gameHistory; /*getParticipatedGames().slice(0);*/
+  var participated_len = participated.length
+  var recent_games = [];
+  if (participated_len >= 0)
+    recent_games.push({
+      id: participated_len - 1,
+      ...participated[participated_len - 1]
+    })
+  if (participated_len >= 1)
+    recent_games.push({
+      id: participated_len - 2,
+      ...participated[participated_len - 2]
+    })
+  console.log('chosed recent game:', recent_games);
+  /*
   let created = getCreatedGames().slice(0);
   let games_num = created.length > participated.length ? created.length : participated.length;
   let recent_games = [];
@@ -186,7 +201,7 @@ function getTwoRecentGames() {
     else
       recent_games.push(participated[game_index]);
   }
-
+  */
   return recent_games;
 
 
@@ -215,7 +230,7 @@ function ShowLastTwoGames(props) {
     },
   }));
 
-  const games = getTwoRecentGames();
+  const games = getTwoRecentGames(props.user);
   const classes = usegameListItemsStyles();
   let url = props.url;
   return (<PrintGames games={games} classes={classes} url={url} />);
@@ -223,41 +238,41 @@ function ShowLastTwoGames(props) {
 }
 
 
-function PrintCreateGameDialog(props){
-    const {handleCloseCreateGameWindow,  createGameWindowOpen, currentUser} = props;
-    const [gameName, setGameName] = React.useState("");
-    const [currentGameNickName, setCurrentGameNickName] = React.useState(currentUser.nickName);
-    let history = useHistory();
-    const startGame = ()=>{
-/*
-        //var user = //todo- get user from session
-        var roomData = {
-          roomName: gameName,
-          user: user
-        }
+function PrintCreateGameDialog(props) {
+  const { handleCloseCreateGameWindow, createGameWindowOpen, currentUser } = props;
+  const [gameName, setGameName] = React.useState("");
+  const [currentGameNickName, setCurrentGameNickName] = React.useState(currentUser.nickName);
+  let history = useHistory();
+  const startGame = () => {
+    /*
+            //var user = //todo- get user from session
+            var roomData = {
+              roomName: gameName,
+              user: user
+            }
+    
+            socket.on('roomOpened', function(roomID){
+              //todo- redirect to room page
+            });
+    
+            socket.emit('openRoom', roomData)
+    */
 
-        socket.on('roomOpened', function(roomID){
-          //todo- redirect to room page
-        });
 
-        socket.emit('openRoom', roomData)
-*/
-        
-
-        console.log("starting game!");
-        console.log("game name:", gameName);
-        console.log('user nickname: ', currentGameNickName);
-        createRoom(gameName, currentUser, currentGameNickName, history);
-        //handleCloseCreateGameWindow();
-        //todo: redirect to room page
-    }
-    return(
-        <Dialog id = "openRoomPopUp" open={createGameWindowOpen} onClose={handleCloseCreateGameWindow} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Create a Room</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-          </DialogContentText>
-          <Grid container spacing={2}>
+    console.log("starting game!");
+    console.log("game name:", gameName);
+    console.log('user nickname: ', currentGameNickName);
+    createRoom(gameName, currentUser, currentGameNickName, history);
+    //handleCloseCreateGameWindow();
+    //todo: redirect to room page
+  }
+  return (
+    <Dialog id="openRoomPopUp" open={createGameWindowOpen} onClose={handleCloseCreateGameWindow} aria-labelledby="form-dialog-title">
+      <DialogTitle id="form-dialog-title">Create a Room</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+        </DialogContentText>
+        <Grid container spacing={2}>
           <Grid item xs={12}>
             <TextField
               autoFocus
@@ -270,13 +285,13 @@ function PrintCreateGameDialog(props){
             />
           </Grid>
           <Grid item xs={12}>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="nickName"
-            label="Nick Name"
-            defaultValue={currentUser.nickName}
-            onChange={(event)=>{
+            <TextField
+              autoFocus
+              margin="dense"
+              id="nickName"
+              label="Nick Name"
+              defaultValue={currentUser.nickName}
+              onChange={(event) => {
                 setCurrentGameNickName(event.target.value);
               }}
             />
