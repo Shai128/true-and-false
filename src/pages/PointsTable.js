f11b738621332fed12cc49eef27c60b6cf9c3d0a
Nadav Elias <nadavelias@gmail.com>
2020-01-20 00:31:01 +0200

    styled score table

--- src/pages/PointsTable.js
+++ src/pages/PointsTable.js
@@ -12,7 +12,8 @@ import { reject } from 'q';
 import { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import {useHistory} from "react-router-dom";
-import { isUndefined, server } from './../Utils.js'
+import { isUndefined, server, colors } from './../Utils.js'
+import Typography from '@material-ui/core/Typography';
 
 // const useStyles = makeStyles({
 //     table: {
@@ -33,13 +34,19 @@ export function PointsTable(props) {
     */
 
     console.log('you have gotten to Points Table!!!', props.location)
-    // const classes = useStyles();
-
+    
+    const [roomId, setRoomId] = useState(props.location.roomId);
     const [rows, setRows] = useState([]);
     let history = useHistory();
     //props = {roomId: 1}
 
-    fetch(server + '/userList/' + props.location.roomId, {
+    // if(roomId === -10){
+    //     console.log("getting room id")
+    //     setRoomId()
+    // }
+
+
+    fetch(server + '/userList/' + roomId, {
       method: 'GET', // *GET, POST, PUT, DELETE, etc.
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
@@ -71,8 +78,20 @@ export function PointsTable(props) {
       console.log("failed. status: ", fail_status)
     })
     
+    let color = colors[Math.floor(Math.random() * colors.length)]
+    let index = Math.floor(Math.random() * 3)
+
     return(
+        <div style={{height:"100vh",
+            background: 'linear-gradient(80deg, ' + color[index % 3] + ' 30%, rgba(0,0,0,0) 30%),'
+              + 'linear-gradient(45deg, ' + color[(index + 1) % 3] + ' 65%, ' + color[(index + 2) % 3] + ' 65%'
+          }}>
         <Grid container justify='center' spacing={2}>
+            <Grid item xs={11}>
+                <Typography variant="h3" align="center" style={{ color: "white", textShadow: "1px 1px 3px black" }}>
+                    Score Table
+                </Typography>
+            </Grid>
             <Grid item xs={11}>
                 <TableContainer component={Paper} align='center'>
                     {/* className={classes.table} */}
@@ -108,5 +127,6 @@ export function PointsTable(props) {
                 </Button>
             </Grid>
         </Grid>
+        </div>
     )
 }
